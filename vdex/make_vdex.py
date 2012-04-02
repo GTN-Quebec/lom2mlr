b030f3f00665c44489a982e7917c79a343d0cf02
Marc-Antoine Parent <maparent@acm.org>
2012-04-02 15:32:05 -0400

    vdex/skos representations

---
+++ vdex/make_vdex.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import sys
+import re
+import argparse
+
+TRE = re.compile('^T[0-9]+$')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='make_vdex')
+    parser.add_argument('--nodef',action='store_true', default=False)
+    parser.add_argument('--lang', action='append', default=['en','fr'])
+    parser.add_argument('voc')
+    args = parser.parse_args()
+    langs = args.lang
+    fname = args.voc
+    if (fname[-4:] != '.txt'):
+        fname += '.txt'
+    vocname = args.voc
+    if (vocname[-4:] == '.txt'):
+        vocname = vocname[:-4]
+    lines = open(fname).readlines()
+    print """<?xml version="1.0" encoding="utf-8"?>
+<vdex:vdex xmlns:vdex="http://www.imsglobal.org/xsd/imsvdex_v1p0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:schemaLocation="http://www.imsglobal.org/xsd/imsvdex_v1p0 http://www.imsglobal.org/xsd/imsvdex_v1p0.xsd" xs:version="0.2">
+  <vdex:vocabIdentifier isRegistered="false">%s</vdex:vocabIdentifier>""" % (vocname)
+    while lines:
+        caps = {}
+        defs = {} 
+        id = lines.pop(0).strip()
+        if not TRE.match(id):
+            sys.stderr.write("error, not TXXX:"+ id)
+            break
+        for lang in langs:
+            l = lines.pop(0).strip()
+            if TRE.match(l):
+                sys.stderr.write("error, TXXX %s while expecting cap of %s"%(l, lang))
+                break
+            caps[lang] = l
+            if not args.nodef:
+                l = lines.pop(0).strip()
+                if TRE.match(l):
+                    sys.stderr.write("error, TXXX %s while expecting def of %s"%(l, lang))
+                    break
+                defs[lang] = l
+        print """  <vdex:term>
+    <vdex:termIdentifier>%s</vdex:termIdentifier>
+    <vdex:caption>""" % (id,)
+        for lang in langs:
+            if caps[lang]:
+                print "        <vdex:langstring language=\"%s\">%s</vdex:langstring>" % (lang, caps[lang])
+        print "    </vdex:caption>"
+        if [x for x in defs.itervalues() if x]:
+            print "    <vdex:description>"
+            for lang in langs:
+                if defs[lang]:
+                    print "        <vdex:langstring language=\"%s\">%s</vdex:langstring>" % (lang, defs[lang])
+            print "    </vdex:description>"
+        print "  </vdex:term>"""
+    print "</vdex:vdex>"
+
---
+++ vdex/vdex2skos.xsl
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" 
+        xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+        xmlns:skos="http://ltsc.ieee.org/xsd/LOM"
+        xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+        xmlns:vdex="http://www.imsglobal.org/xsd/imsvdex_v1p0"
+        extension-element-prefixes="vdex"
+        >
+    <xsl:output method="xml" encoding="UTF-8"/>
+
+    <xsl:variable name="voc" select="/vdex:vdex/vdex:vocabIdentifier/text()" />
+
+    <xsl:template match="/">
+        <xsl:apply-templates />
+    </xsl:template>
+
+    <xsl:template match="vdex:vdex">
+        <rdf:RDF>
+            <xsl:apply-templates />
+        </rdf:RDF>
+    </xsl:template>
+
+    <xsl:template match="text()" />
+    <xsl:template match="text()"  mode="caption"/>
+    <xsl:template match="text()"  mode="description"/>
+
+    <xsl:template match="vdex:term">
+        <skos:Concept>
+            <xsl:attribute name="rdf:about">
+                <xsl:value-of select="concat($voc,'#',vdex:termIdentifier/text())"/>
+            </xsl:attribute>
+            <xsl:apply-templates />
+        </skos:Concept>
+    </xsl:template>
+
+    <xsl:template match="vdex:caption">
+            <xsl:apply-templates mode="caption"/>
+    </xsl:template>
+    <xsl:template match="vdex:description">
+            <xsl:apply-templates mode="description"/>
+    </xsl:template>
+    <xsl:template match="vdex:langstring" mode="caption">
+        <skos:prefLabel>
+            <xsl:attribute name="xml:lang">
+                <xsl:value-of select="@language"/>
+            </xsl:attribute>
+            <xsl:value-of select="text()"/>
+        </skos:prefLabel>
+    </xsl:template>
+    <xsl:template match="vdex:langstring" mode="description">
+        <skos:definition>
+            <xsl:attribute name="xml:lang">
+                <xsl:value-of select="@language"/>
+            </xsl:attribute>
+            <xsl:value-of select="text()"/>
+        </skos:definition>
+    </xsl:template>
+
+</xsl:stylesheet>
