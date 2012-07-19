bd41aa59097f4b8f8d350e40d1260608b4cbbbb5
Marc-Antoine Parent <maparent@acm.org>
2012-07-19 16:05:41 -0400

    args in splitcode

--- util.py
+++ util.py
@@ -3,15 +3,18 @@ def is_sequence(arg):
             hasattr(arg, "__getitem__") or
             hasattr(arg, "__iter__"))
 
+
 def unlist(arg):
     if is_sequence(arg) and len(arg) == 1:
         return arg[0]
     return arg
 
+
 def seqlen(arg):
     if is_sequence(arg):
         return len(arg)
     return 1
+
 
 def unwrap_seq(func):
     def wrapped(context, *args):
@@ -31,6 +34,7 @@ def unwrap_seq(func):
             return func(context, *args)
     return wrapped
 
+
 def splitcode(code):
     if code[0:3] == ':::':
         lines = code.split("\n")
@@ -38,4 +42,7 @@ def splitcode(code):
         code = "\n".join(lines[1:])
     else:
         code_type = None
-    return code_type, code
+    args = None
+    if ' ' in code_type:
+        code_type, args = code_type.split(' ', 1)
+    return code_type, code, args
