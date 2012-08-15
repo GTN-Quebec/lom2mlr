c8fc071c690ea87ca60aac8da62310c29457ed91
Marc-Antoine Parent <maparent@acm.org>
2012-08-15 15:03:09 -0400

    simplify vdex

--- lom2mlr/vdex/make_vdex.py
+++ lom2mlr/vdex/make_vdex.py
@@ -2,26 +2,19 @@
 
 import sys
 import re
-import argparse
 
 TRE = re.compile('^T[0-9]+$')
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='make_vdex')
-    parser.add_argument('--nodef', action='store_true', default=False)
-    parser.add_argument('--lang', action='append', default=['eng', 'fra'])
-    parser.add_argument('voc')
-    args = parser.parse_args()
-    langs = args.lang
-    fname = args.voc
-    if (fname[-4:] != '.txt'):
-        fname += '.txt'
-    vocname = args.voc
-    if (vocname[-4:] == '.txt'):
-        vocname = vocname[:-4]
-    if vocname.rsplit('_', 1)[-1] in ('rus', 'nodef'):
-        vocname = vocname.rsplit('_', 1)[0]
+    vocname = sys.argv[0]
+    fname = sys.argv[0]
+    vocname = fname.rsplit('.', 1)[0]
     lines = open(fname).readlines()
+    langs = lines.pop(0).split()
+    hasdef = True
+    if langs[0] == 'nodef':
+        hasdef = False
+        langs.pop(0)
     print """<?xml version="1.0" encoding="utf-8"?>
 <vdex:vdex xmlns:vdex="http://www.imsglobal.org/xsd/imsvdex_v1p0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:schemaLocation="http://www.imsglobal.org/xsd/imsvdex_v1p0 http://www.imsglobal.org/xsd/imsvdex_v1p0.xsd" xs:version="0.2">
   <vdex:vocabIdentifier isRegistered="false">%s</vdex:vocabIdentifier>""" % (vocname)
@@ -38,7 +31,7 @@ if __name__ == '__main__':
                 sys.stderr.write("error, TXXX %s while expecting cap of %s" % (l, lang))
                 break
             caps[lang] = l
-            if not args.nodef:
+            if hasdef:
                 l = lines.pop(0).strip()
                 if TRE.match(l):
                     sys.stderr.write("error, TXXX %s while expecting def of %s" % (l, lang))
